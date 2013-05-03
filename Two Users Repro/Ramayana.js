d1dc43a0bf87f74ef6167bfd87d05d57b9ed744e
dlongshore@nimblepros.com <dlongshore@nimblepros.com>
2013-05-03 17:39:05 -0500

    Ramayana returns smallest perfect number.

--- Two Users Repro/Ramayana.js
+++ Two Users Repro/Ramayana.js
@@ -1,2 +1,2 @@
  "The Sage-Poet Valmiki is inquisitively enquiring about a man who is a composite for all merited endowments."
-return 2 + 2; //more dynamic
+return 6; //perfect number: 1 * 2 * 3 = 6 and 1 + 2 + 3 = 6
